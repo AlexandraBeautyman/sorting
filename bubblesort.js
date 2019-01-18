ff364bd01a6e3930971d7bfda2d2949140e3e7ed
Natia Khomasuridze <natiakhoma@gmail.com>
2019-01-18 16:53:14 -0500

    fd

--- bubblesort.js
+++ bubblesort.js
@@ -1,12 +1,27 @@
 function bubbleSort(array) {
+    let bool = true;
     if (array.length !== 0 && array.length !== 1) {
+        if(!bool){
+            break;
+        }
         for (let i = 0; i < array.length; i++) {
             for (let j = 0; j < array.length - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     // swap the two
+                    const temp = array[j]
+                    array[j]= array[j+1]
+                    array[j+1]=temp
                 }
+                else if( ){
+
+                }
+                
             }
         }
     }
     return array;
 }
+
+// var b = list[y];
+// list[y] = list[x];
+// list[x] = b;
